484d938c5c1b7b6381559f8dc04b6f9a0768001f
meganrm <meganrm@gmail.com>
2017-01-22 16:53:51 -0500

    pop up only if no user

--- scripts/event.js
+++ scripts/event.js
@@ -19,30 +19,31 @@
 
   var provider = new firebase.auth.GoogleAuthProvider();
 
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    console.log(user);
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode, errorMessage);
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+  Event.signIn = function (){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+    });
+  }
+
 
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
     // User is signed in.
-      console.log('user is signed in');
+      console.log(user.displayName, ' is signed in');
     } else {
+      Event.signIn()
       // No user is signed in.
     }
   });
